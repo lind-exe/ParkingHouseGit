7a53191944c7691b42d77638f773e575b460e887
lind-exe <jojje_lind@live.se>
2022-12-06 09:32:33 +0100

    initial commit
    hej

---
+++ ParkingHouse/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ParkingHouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Hejhej");
+        }
+    }
+}
